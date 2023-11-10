4830cb10332187a92561a2a47e362641b0da881f
Diego Mesa <dhiego__07@hotmail.com>
2023-11-10 17:00:15 -0500

    Ajuste de back end con arquitectura

---
+++ part3/controllers/person.js
@@ -0,0 +1,67 @@
+const personRouter = require('express').Router()
+const Person = require('../models/person')
+
+/* ******************************* NOTAS ******************************* */
+
+
+
+
+/* ******************************* PERSONAS ******************************* */
+
+personRouter.get('/', (request, response) => {
+  Person.find({}).then(person => {
+    response.json(person)
+  })
+})
+
+personRouter.get('/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+personRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+personRouter.delete('/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+personRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = personRouter
